c0f9e665643807871a21d36f5a7222256f455d24
Vedant Lihitkar <vedantlihitkar@gmail.com>
2026-09-24 01:03:56 +0530

    added libraries needed in this prj

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("helo")
+import pandas as pd
+import numpy as np
+import missingno as msno
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
+from sklearn import metrics
+from sklearn.metrics import roc_curve
+from sklearn.metrics import recall_score, confusion_matrix, precision_score, f1_score, accuracy_score, classification_report
+
+
+df=pd.read_csv(r'D:\customer-churn-prediction\data.csv')
+
+# print(df.head(10))
+# print(df.sample(10))
+
+# print(df.shape)
+# print(df.columns)
+
+
+print(df.info())
+
+
+# target value-churn - yes/no
